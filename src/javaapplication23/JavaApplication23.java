f7d7b0a6329e6d2c4f61eb441f61aaa636c05992
Iago Pampillón Vieitez <ipampillonvieitez@nautilus27.ad.danielcastelao.org>
2019-10-11 14:10:44 +0200

    first commit

--- src/javaapplication23/JavaApplication23.java
+++ src/javaapplication23/JavaApplication23.java
@@ -16,7 +16,7 @@ public class JavaApplication23 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hola mundo");
+        System.out.println("chao mundo");
     }
     
 }
